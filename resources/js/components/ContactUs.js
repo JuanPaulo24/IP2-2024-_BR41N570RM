628f9030590f869c39b3a572cf9ac3ca09a52ef8
JuanPaulo24 <castrodesjohnpaul@gmail.com>
2024-09-22 11:12:00 +0800

    IP2 resource

---
+++ resources/js/components/ContactUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NavList from "./NavList"; 
+
+export default function ContactUs() {
+    return (
+        <div className="contactus">
+            <NavList />
+            <h1>Contact Us</h1>
+        </div>
+    );
+}
---
+++ resources/js/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NavList from "./NavList"; 
+
+export default function Home() {
+    return (
+        <div className="home">
+            <NavList />
+            <h1>Welcome to Laravel Home</h1>
+        </div>
+    );
+}
