5e7b8969141c968c8b03d32d39b34fa8f17254ba
NazirBuenavidez <nazzbuenavidez@hotmail.com>
2024-10-02 21:30:31 +0800

    Added UI programs

---
+++ resources/js/components/Programs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavList from "./NavList"; 
+
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <NavList />
+            <h1>3</h1>
+            <p>Welcome to this Website! Thank you for Visiting!</p>
+        </div>
+    );
+}
--- resources/js/components/Routers.js
+++ resources/js/components/Routers.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-import Demo from "./Demo";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Programs from "./Programs";
+import Contacts from "./Contacts";
 
 export default function Routers() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Demo />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/programs" element={<Programs />} />
+                <Route path="/contacts" element={<Contacts />} />
             </Routes>
         </Router>
     );
